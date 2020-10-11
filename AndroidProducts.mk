28e42c32cd1c7fab2b0fd75c58ff5699e6b7ca50
jmpfbmx <jmpf.bmx@gmail.com>
2020-10-11 23:23:25 +0200

    wiam_24: AEX-ify!

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,2 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_wiam_24.mk
+    $(LOCAL_DIR)/aosp_wiam_24.mk
--- aosp_wiam_24.mk
+++ aosp_wiam_24.mk
@@ -23,5 +23,30 @@ PRODUCT_COPY_FILES := device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
+# Release name
+PRODUCT_RELEASE_NAME := wiam_24
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/aosp/common.mk)
+
+# BootAnimation
+TARGET_BOOT_ANIMATION_RES := 480
+
+# Inherit device configuration
 $(call inherit-product, device/wolder/wiam_24/device_wiam_24.mk)
 $(call inherit-product-if-exists, vendor/wolder/wiam_24/device-vendor.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := wiam_24
+PRODUCT_NAME := aosp_wiam_24
+PRODUCT_BRAND := wolder
+PRODUCT_MODEL := wolder Wiam 24
+PRODUCT_MANUFACTURER := wolder
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BUILD_FINGERPRINT=Wolder/full_aeon6580_we_m/aeon6580_we_m:6.0/MRA58K/1466685788:user/dev-keys \
+    PRIVATE_BUILD_DESC="full_aeon6580_we_m-user 6.0 MRA58K 1466685788 dev-keys"
+
+# SuperUser
+WITH_SU := false
+WITH_ROOT := false
